5a86a034a2df21c0d57475972ac4f6b0e4c5613c
npg2108 <npg2108@cumc.columbia.edu>
2018-08-08 12:53:02 -0400

    Modified app code and added links to each feature

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config.supress_callback_exceptions=True
 
 #####load and process data#####
 
-data = feather.read_feather(source='data/aeolus_top5drugs.feather',nthreads=16,columns=['drug_concept_name','age_cat'])
+data = feather.read_feather(source='data/aeolus_top5drugs.feather',nthreads=16)
 
 uniq_drugs = data.drug_concept_name.unique()
 uniq_drugs = uniq_drugs[np.argsort(uniq_drugs)]
@@ -30,7 +30,6 @@ all_age_cat_counts_y_norm = np.round((all_age_cat_counts_y / all_age_cat_counts.
 
 ##########
 
-
 app.layout = html.Div(
 
 	children=[
@@ -42,60 +41,142 @@ app.layout = html.Div(
 	
 	
 index_page = html.Div(
-	
-	style={'font-family' : 'Verdana'
-	        },
 	children=[
 
 		html.H1('Visualizing Drug Safety Data in a Web Framework: Interacting with, understanding, and communicating using Python',
 			style={'text-align' : 'center','font-size' : 48}
 			),
 
-		dcc.Link('Go to ADR table Page', href='/table_page',
+		dcc.Link('Go to ADR age relation Page', href='/age_page',
 			style={'font-size' : 16,'color' : 'blue'}),
 
 		html.Br(),
 
-		dcc.Link('Go to ADR age relation Page', href='/age_page',
+		dcc.Link('Investigate Drugs',
+			href = '/feature_drugs_page',
 			style={'font-size' : 16,'color' : 'blue'}),
+
+		html.Br(),
+
+		dcc.Link('Investigate Adverse Reactions',
+			href = '/feature_outcomes_page',
+			style={'font-size' : 16,'color' : 'blue'}),
+
+		html.Br(),
+
+		dcc.Link('Investigate ADRs',
+			href = '/feature_drugs_and_outcomes_page',
+			style={'font-size' : 16,'color' : 'blue'}),
+
+		html.Br(),
 
 		html.Hr(),
 
-		dcc.Markdown(dedent('''
+		html.H4("The Food and Drug Administration's Adverse Event Reporting System provides population-level information on drug usage and adverse reactions to drugs (ADRs). We present a standardized, processed version of this ADR data for the public."),
 
-			**Academic researchers extensively curate datasets that are of interest to the general public. Often, these data and results from research projects using this data appear in Academic journals as publications, which are the main modes of research communication, funding eligibility, and career advancement. However, research publications are esoteric and are not feasible for widespread communication to the public. Thus, there requires a medium that allows for general communication of research results, along with scholarly communication in peer-reviewed journals.**
+		html.Br(),
 
-			**Dash, from Plotly, is a web framework for interacting and communicating data using python. This app uses Dash to communicate and make interactive data curated from the Federal Drug Administration's Adverse Event Reporting System.**
+		html.H4("Feel free to use the Jupyter notebooks in our github repository SafeDrugs (www.github/NCBI-Hackathons/SafeDrugs) for the data wrangling and code implementation."),
 
-			'''))
+		html.Br(),
+
+		html.H4("Check out the individual fveature links to investigate frequent drugs, adverse reactions, and ADRs. Post issues on our github SafeDrugs (www.github/NCBI-Hackathons/SafeDrugs) for additional features or bugs."),
+
+		html.Br()
 		]
 	)
 
-
-table_page = html.Div(
-
-	style={},
+feature_drugs_page = html.Div(
 
 	children=[
 
-		html.H1('Table Page'),
+		html.H1('Investigating Drug usage \nin ADR data',
+			style={'text-align' : 'center'}),
 
 		dcc.Link('Go to Home Page', href='/',
 			style={'font-size' : 16,'color' : 'blue'}),
 
 		html.Br(),
 
-		dcc.Link('Go to ADR age relation Page', href='/age_page',
+		dcc.Link('Investigate Adverse Reactions',
+			href = '/feature_outcomes_page',
 			style={'font-size' : 16,'color' : 'blue'}),
 
-		html.Hr()
+		html.Br(),
+
+		dcc.Link('Investigate ADRs',
+			href = '/feature_drugs_and_outcomes_page',
+			style={'font-size' : 16,'color' : 'blue'}),
+
+		html.Br(),
+
+		html.Hr(),
+
+		html.Div(style={},children=[])
 		]
 	),
 
+feature_outcomes_page = html.Div(
+
+	children=[
+
+		html.H1('Investigating Adverse Reactions from drug usage \nin ADR data',
+			style={'text-align' : 'center'}),
+		
+		dcc.Link('Go to Home Page', href='/',
+			style={'font-size' : 16,'color' : 'blue'}),
+
+		html.Br(),
+
+		dcc.Link('Investigate Drugs',
+			href = '/feature_drugs_page',
+			style={'font-size' : 16,'color' : 'blue'}),
+
+		html.Br(),
+
+		dcc.Link('Investigate ADRs',
+			href = '/feature_drugs_and_outcomes_page',
+			style={'font-size' : 16,'color' : 'blue'}),
+
+		html.Br(),
+
+		html.Hr(),
+
+		html.Div(style={},children=[])
+		]
+	),
+
+feature_drugs_and_outcomes_page = html.Div(
+
+	children=[
+
+		html.H1('Investigating ADRs',
+			style={'text-align' : 'center'}),
+		
+		dcc.Link('Go to Home Page', href='/',
+			style={'font-size' : 16,'color' : 'blue'}),
+
+		html.Br(),
+
+		dcc.Link('Investigate Drugs',
+			href = '/feature_drugs_page',
+			style={'font-size' : 16,'color' : 'blue'}),
+
+		html.Br(),
+
+		dcc.Link('Investigate Adverse Reactions',
+			href = '/feature_outcomes_page',
+			style={'font-size' : 16,'color' : 'blue'}),
+
+		html.Br(),
+
+		html.Hr(),
+
+		html.Div(style={},children=[])
+		]
+	),
 
 age_page = html.Div(
-
-	style={},
 
 	children=[
 
@@ -124,14 +205,14 @@ age_page = html.Div(
 				options=[{'label' : i, 'value' : i} for i in uniq_drugs],
 				value=uniq_drugs[0]),
 
-    		html.Div(id='drug_output',
-    			style={'text-align' : 'center'})
+			html.Div(id='drug_output',
+				style={'text-align' : 'center'})
 
 			]),
 
 		dcc.Graph(
-	        id='drug-reports-at-ages',
-	        style={'class' : 'col-sm-4','float' : 'right'},
+			id='drug-reports-at-ages',
+			style={'class' : 'col-sm-4','float' : 'right'},
 		)
 
 		]
@@ -147,24 +228,28 @@ age_page = html.Div(
 # corresponding page. 
 @app.callback(
 	dash.dependencies.Output('page-content', 'children'),
-              [dash.dependencies.Input('url', 'pathname')])
+			  [dash.dependencies.Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/table_page':
-    	return table_page
-    if pathname == '/age_page':
-    	return age_page
-    else:
-        return index_page
+	if pathname == '/age_page':
+		return age_page
+	if pathname == '/feature_drugs_page':
+		return feature_drugs_page
+	if pathname == '/feature_outcomes_page':
+		return feature_outcomes_page
+	if pathname == '/feature_drugs_and_outcomes_page':
+		return feature_drugs_and_outcomes_page
+	else:
+		return index_page
 
 
 #Show number of patients taking selected drug in dataset
 @app.callback(
-    dash.dependencies.Output('drug_output', 'children'),
-    [dash.dependencies.Input('drug_count', 'value')])
+	dash.dependencies.Output('drug_output', 'children'),
+	[dash.dependencies.Input('drug_count', 'value')])
 def callback_drug(value):
-    return 'There are {} patients that reported taking {}'.format(
-    	data.query('drug_concept_name==@value').count().values[0],
-    	value)
+	return 'There are {} patients that reported taking {}'.format(
+		data.query('drug_concept_name==@value').count().values[0],
+		value)
 
 #Update bar graph for drug
 @app.callback(
@@ -181,50 +266,50 @@ def callback_drug_reports_at_ages_bars(value):
 	y_norm = np.round((series.values / series.sum()) * 100,0)
 
 	drug_trace = go.Bar(
-		                x=x,
-		                y=y_norm,
-		                name='{}'.format(value),
-		                text=['{} reports'.format(i) for i in y],
-		                marker=go.Marker(
-		                    color='rgb(55, 83, 109)'
-		                )
-		            )
+						x=x,
+						y=y_norm,
+						name='{}'.format(value),
+						text=['{} reports'.format(i) for i in y],
+						marker=go.Marker(
+							color='rgb(55, 83, 109)'
+						)
+					)
 
 	all_trace = go.Bar(
-		                x=all_age_cat_counts_x,
-		                y=all_age_cat_counts_y_norm,
-		                name='All drugs',
-		                text=['{} reports'.format(i) for i in all_age_cat_counts_y],
-		                marker=go.Marker(
-		                    color='rgb(180,180,180)'
-		                )
-		            )
+						x=all_age_cat_counts_x,
+						y=all_age_cat_counts_y_norm,
+						name='All drugs',
+						text=['{} reports'.format(i) for i in all_age_cat_counts_y],
+						marker=go.Marker(
+							color='rgb(180,180,180)'
+						)
+					)
 	return { 'data' : [
-		            all_trace,drug_trace
-		            ],
-	            'layout' : go.Layout(
-		            title='Patients taking {} at different age intervals'.format(value),
-		            showlegend=True,
-		            yaxis = dict(
-		            	title="Percentage of reports",
-		            	type='-'
-		            ),
-		            xaxis = dict(
-		            	title="Age category"
-		            ),
-		            legend=go.Legend(
-		                x=0,
-		                y=1.0
-		            ),
-		            margin=go.Margin(l=40, r=20, t=40, b=100)
-	        	)
+					all_trace,drug_trace
+					],
+				'layout' : go.Layout(
+					title='Patients taking {} at different age intervals'.format(value),
+					showlegend=True,
+					yaxis = dict(
+						title="Percentage of reports",
+						type='-'
+					),
+					xaxis = dict(
+						title="Age category"
+					),
+					legend=go.Legend(
+						x=0,
+						y=1.0
+					),
+					margin=go.Margin(l=40, r=20, t=40, b=100)
+				)
 			}
 
 #enable bootstrap styling
-external_css = ["https://bootswatch.com/3/paper/bootstrap.css"]
+external_css = [["https://bootswatch.com/3/paper/bootstrap.css"]]
 
 for css in external_css:
-    app.css.append_css({"external_url": css})
+	app.css.append_css({"external_url": css})
 
 
 if __name__ == '__main__':
